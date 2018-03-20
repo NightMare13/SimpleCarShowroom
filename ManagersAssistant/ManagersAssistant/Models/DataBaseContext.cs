e906cf3e49e67eda30c90720e4c6aaa928eb1543
NightMare13 <36300177+NightMare13@users.noreply.github.com>
2018-03-21 02:53:14 +0300

    Add database

---
+++ ManagersAssistant/ManagersAssistant/Models/DataBaseContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ManagersAssistant.Models
+{
+    public class DataBasesContext : DbContext
+    {
+        public DataBasesContext() : base("DataBaseConnection")
+        {
+            
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ ManagersAssistant/ManagersAssistant/Models/ProductsDBInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ManagersAssistant.Models
+{
+    public class ProductsDBInitializer :DropCreateDatabaseAlways<DataBasesContext>
+    {
+        protected override void Seed(DataBasesContext context)
+        {
+            context.Cars.Add(new Car());
+            context.Customers.Add(new Customer());
+            context.Orders.Add(new Order());
+
+            base.Seed(context);
+        }
+    }
+}
